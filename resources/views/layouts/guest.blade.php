687896478e88d3cc827f542c2cfafe3f56816eec
Yasin Tuğrul <darkovy@gmail.com>
2026-02-10 02:53:33 +0300

    Remove old favicon.ico and link new favicon.png

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Erpovy Kurumsal Yönetim Sistemi V2') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
