6b79b71fcdc61b6ef2f26b13ebbb0fb678313c07
Yasin Tuğrul <darkovy@gmail.com>
2026-02-10 02:16:39 +0300

    Redirect root route to login page

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
