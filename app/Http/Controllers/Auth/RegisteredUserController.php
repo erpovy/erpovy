7e8d6b0c66fe73d290152beabcb869036616c127
Yasin Tuğrul <darkovy@gmail.com>
2026-02-11 23:32:29 +0300

    \fix: remove duplicate user creation in RegisteredUserController"

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -52,13 +52,6 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        $user = User::create([
-            'company_id' => $company->id,
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
         event(new Registered($user));
 
         Auth::login($user);
